eeccbe719deda0c588dfa2e52d9f26e1bfea7af0
Nehal Ali <nehalalikamran103@gmail.com>
2025-10-31 09:18:20 -0700

    prompt box paracitics file

---
+++ Class-26/app.js
@@ -0,0 +1,18 @@
+
+
+var number1 = +prompt("Enter you first Value" );
+var number2 = +prompt("Enter you second Value" );
+
+var sum = number1 + number2;
+
+alert(sum);
+
+
+// console.log(typeof number1);
+
+var name = prompt("Enter you First Name");
+var lastname = prompt("Enter you Last Name");
+
+var value_1 = name + lastname;
+
+alert (value_1);
