a2d0fbd1594575f182c0353c445f9e612e2b4550
Nehal Ali <nehalalikamran103@gmail.com>
2025-10-31 12:31:32 -0700

    class 25

---
+++ Class-25/app.js
@@ -0,0 +1,19 @@
+
+// var gender = prompt("Enter your gender", "male")
+
+// var age = prompt ("Enter your age 18");
+
+if (age == 18) {
+    alert("you are 18 age old Hm")
+}
+
+var password = prompt("Enter you password");
+var reEnterPassword = prompt("Re-Enter you password");
+
+if (reEnterPassword != password){
+    alert("Wrong password");
+}
+
+if(password == reEnterPassword){
+    alert("successfully password");
+}
