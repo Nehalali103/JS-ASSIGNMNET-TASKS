d413f75f52c73b0240406158684f683d5d5d5d43
Nehal Ali <nehalalikamran103@gmail.com>
2025-11-12 01:22:01 -0800

    logical operator task

---
+++ Class-27/app.js
@@ -0,0 +1,23 @@
+
+
+// var placeName = prompt("Enter your place name");
+// var age = +prompt("Enter are age");
+
+// if ( placeName != "" && age != "" ) {
+//     console.log("you are eiligble")
+// }
+
+// else {
+//     console.log("not are eiligble")
+// }
+
+
+var myName = prompt('Enter your name');
+
+if ( myName == "nehalali" || myName == "abdullahali" ) {
+    console.log( myName , true);
+}
+
+else {
+    console.log (false);
+};
