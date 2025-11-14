30b4de350eb6cd741ff7ec415c7553976166a58e
Nehal Ali <nehalalikamran103@gmail.com>
2025-11-14 08:12:05 -0800

    ok

---
+++ Class-28/app.js
@@ -0,0 +1,53 @@
+
+
+// var cleancites1 = 'Islamabad';
+// var cleancites2 = "Lahore";
+// var cleancites3 = 'Multan';
+// var cleancites4 = "Karachi"
+// var cleancites5 = "pushwar"
+
+// var arr = [cleancites1 , cleancites2 , cleancites3 , cleancites4 , cleancites5]
+
+
+// var myArr = ['Nehal', 'Ayan' ,'Uzair', 'Ahmed', 'Kamil Sir', arr]
+
+// var allArr = myArr
+
+// console.log(allArr[5][3]);
+
+
+
+
+// var usersName = prompt("Enter your my name ")
+
+// if ( usersName == 'Arham' ||
+//      usersName == 'Nehal'  )
+// {
+//     // console.log("found susscful go")
+//     var askName = prompt('nata kya kar na ha')
+//     if ( askName == "parata" || askName == "tea") {
+//         console.log("le ao bhai")
+//     }
+//     else {
+//         console.log("nahi kar na")
+//     } 
+// }
+
+// else {
+//     console.log("not name found")
+// };
+
+
+
+
+
+var arr = ["Majid" , "Faiz", "Arham"]
+
+arr [3] = "Nehal"
+arr [0] = 'Ali'
+arr [0] = 'Majid Ali'
+
+console.log(arr)
+
+
+
