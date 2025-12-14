39135c774866bc90da204474f21d9d0fa966bf27
Nehal Ali <nehalalikamran103@gmail.com>
2025-12-14 02:52:48 -0800

    .

--- login form js/app.js
+++ login form js/app.js
@@ -1,9 +1,31 @@
-var email = document.getElementById("email")
-var pass = document.getElementById("pass")
+var email = document.getElementById("email").innerHTML
+var pass = document.getElementById("pass").innerHTML
 
+function sweetAlert2(message) {
+
+    // console.log(message)
+    Swal.fire({
+        icon: `error`,
+        title: "Oops...",
+        text: "Something went wrong!",
+        footer: `<a href="#">${message}!</a>`
+    });
+}
 
 function submitHandle(e) {
-    console.log("Form submit horaha ha")
-    // e.javaScriptvoid(0)
-    e.prevenDefault()
+    // console.log("form submit horaha hai ...", e)
+    //     // console.log("Form submit horaha ha")
+    //     // e.javaScriptvoid(0)
+    //     // e.window.location.reload()
+    e.preventDefault()
+
+    if ( email.value.trim() == "") {
+        sweetAlert2("Please Enter a Email!")
+        // return
+
+    }
 }
+
+
+
+
