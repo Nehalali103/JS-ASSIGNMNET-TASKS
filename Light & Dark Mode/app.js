e537c056b8148b39f68ab087f1f9846281ea14e2
Nehal Ali <nehalalikamran103@gmail.com>
2025-12-19 10:52:13 -0800

    dark & light M ode tak

---
+++ Light & Dark Mode/app.js
@@ -0,0 +1,13 @@
+
+var bdEl = document.body
+var btnEl = document.getElementById("btn")
+
+function modify(){
+    bdEl.style.backgroundColor='black'
+    // btnEl.innerHTML = 'Light Mode'
+}
+
+function unModify(){ 
+    bdEl.style.backgroundColor='white'
+    // btnEl.innerHTML = 'Dark Mode'
+}
