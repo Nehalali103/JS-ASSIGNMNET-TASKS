7ee47e64728dc1cbb3d264ffa435cb1831c81275
Nehal Ali <nehalalikamran103@gmail.com>
2025-12-21 22:15:11 -0800

    complete ho ga ya

--- Game Task/app.js
+++ Game Task/app.js
@@ -1,6 +1,6 @@
 var left = 0;
 function abc() {
-    // console.log(event.keyCode)
+    console.log(event.keyCode)
     var charcter = document.getElementById('charcter')
     if (event.keyCode === 68 && left < 1100) {
         left = left + 10;
@@ -18,11 +18,19 @@ function abc() {
         charcter.style.height = '400px'
         charcter.src = 'iron-gem.gif'
         setTimeout(function () {
-            charcter.src = 'iron-man-stance.gif'
             charcter.style.width = '250px'
             charcter.style.height = '250px'
-
+            charcter.src = 'iron-man-stance.gif'
+            
         }, 2500)
+    }
+    if ( event.keyCode === 82 ) {
+        charcter.src = 'ironman-shield.gif'
+        setTimeout(function(){
+            charcter.style.width = '250px'
+            charcter.style.height = '250px'
+            charcter.src = "iron-man-stance.gif"
+        }, 800)
     }
 
 }
