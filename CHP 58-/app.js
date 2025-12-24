1ab6299487fd954bc446cc732643a560b94e7f51
Nehal Ali <nehalalikamran103@gmail.com>
2025-12-24 01:42:11 -0800

    56 se 58 ho gaye

--- CHP 58-/app.js
+++ CHP 58-/app.js
@@ -1,17 +1,102 @@
 // console.log('js file run');
 
-var paraEle = document.getElementsByTagName('p')
-
-var para =  paraEle.style. = 'red'
-
-console.log(para)
-
-// document.getElementsByName('p').style.color='blue'
 
 
 
-function manipulatePara() {
-  var paras = document.getElementsByTagName("p");
 
-  console.log("paras ==> ", paras)
-};
+
+// var liEl = document.getElementsByTagName('li')
+
+// // console.log(liEl[0])
+
+// for (i = 0; i < liEl.length; i++) {
+//     liEl[i].style.color = 'red'
+//     if ( )
+// }
+
+
+
+
+// function manipulatePara() {
+//     var btn = document.getElementById('c3')
+//     var paras = document.getElementsByName("p");
+
+//     //   var para = paras.style.color ='red'
+
+//     console.log("paras ==> ", paras)
+
+
+//     for (var i = 0; i <= paras.length; i++) {
+//         paras[i].className += "red"
+//     };
+
+// };
+
+
+
+
+// Q no 1
+
+// var paraEl = document.getElementsByTagName('p')
+
+// paraEl[1].innerHTML = "This  SUV is too big."
+
+// console.log(paraEl)
+
+// for (var i = 0; i < paraEl.length; i++) {
+//     paraEl[i].style.color = 'blue';
+//     paraEl[i].style.fontSize = '20px';
+// }
+
+
+// // Q no 2
+
+// var liEl = document.getElementsByTagName('li')
+
+// // console.log(liEl)
+// for (var i = 0; i < liEl.length; i++){
+//     liEl[i].style.color = 'red';
+//     liEl[i].style.fontSize = '20px';
+// }
+
+
+// Q no 3 ???
+// var e = document.getElementById('paraE')
+
+// var newPara = e.document.getElementsByTagName('p')
+
+// console.log(newPara)
+
+// {
+//     for (var i = 0; i < newPara.lenght; i++)
+//         newPara[i].style.color = 'red'
+// }
+
+
+
+var assces = document.childNodes[1].childNodes[2].childNodes[9].childNodes[1];
+
+console.log(assces)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
