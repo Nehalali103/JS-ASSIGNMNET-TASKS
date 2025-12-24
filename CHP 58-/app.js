a6cce6ccbe7d5d2d6f37a60ba02d9a1b45565c82
Nehal Ali <nehalalikamran103@gmail.com>
2025-12-23 22:52:57 -0800

    JS

---
+++ CHP 58-/app.js
@@ -0,0 +1,17 @@
+// console.log('js file run');
+
+var paraEle = document.getElementsByTagName('p')
+
+var para =  paraEle.style. = 'red'
+
+console.log(para)
+
+// document.getElementsByName('p').style.color='blue'
+
+
+
+function manipulatePara() {
+  var paras = document.getElementsByTagName("p");
+
+  console.log("paras ==> ", paras)
+};
