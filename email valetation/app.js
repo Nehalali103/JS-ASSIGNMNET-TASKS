f35a48622d5fb962f0259a080367b8213e6b278d
Nehal Ali <nehalalikamran103@gmail.com>
2026-01-09 07:06:49 -0800

    DOne

--- email valetation/app.js
+++ email valetation/app.js
@@ -1,15 +1,24 @@
+// let resultval = document.getElementById('result')
+// resultval.innerHTML = 'Email Approved '
+// let form = document.getElementById('form')
+// resultval.innerHTML = 'Invaild Email Adderss' 
 
 function check(){
     let form = document.getElementById("form")
     let emalEl = document.getElementById('email').value
-    let resultval = document.getElementById('result')
-    const  regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+    const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,5}$/
 
-    if(regex.test(emalEl)){
-        resultval.innerHTML = 'Email Approved '
+
+    if (regex.test(emalEl) ) {
         form.classList.add('vaild')                        
+        form.classList.remove('invaild')                        
     }
     else{
-        resultval.innerHTML = 'Invaild Email Adderss' 
+        form.classList.add('invaild')                        
+        form.classList.remove('vaild')                        
+    }
+    if(emalEl == ''){
+        form.classList.remove('vaild')                        
+        form.classList.remove('invaild')                        
     }
 }
