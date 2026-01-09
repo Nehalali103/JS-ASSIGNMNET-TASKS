425314d380c6a61a40938db0c870455d6adf3889
Nehal Ali <nehalalikamran103@gmail.com>
2026-01-09 07:51:19 -0800

    second time  done

--- email valetation/app.js
+++ email valetation/app.js
@@ -3,22 +3,61 @@
 // let form = document.getElementById('form')
 // resultval.innerHTML = 'Invaild Email Adderss' 
 
-function check(){
-    let form = document.getElementById("form")
-    let emalEl = document.getElementById('email').value
+// function check(){
+//     let form = document.getElementById("form")
+//     let emalEl = document.getElementById('email').value
+//     const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,5}$/
+
+
+//     if (regex.test(emalEl) ) {
+//         form.classList.add('vaild')                        
+//         form.classList.remove('invaild')                        
+//     }
+//     else{
+//         form.classList.add('invaild')                        
+//         form.classList.remove('vaild')                        
+//     }
+//     if(emalEl == ''){
+//         form.classList.remove('vaild')                        
+//         form.classList.remove('invaild')                        
+//     }
+// }
+
+
+
+function inp(){
+    let emailEl = document.getElementById('email').value
+    let d = document.getElementById('indication')
+    let form = document.getElementById('form')
     const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,5}$/
 
-
-    if (regex.test(emalEl) ) {
-        form.classList.add('vaild')                        
-        form.classList.remove('invaild')                        
+    if( regex.test(emailEl) ){
+        form.classList.add('valid')
+        form.classList.remove('invalid')
     }
     else{
-        form.classList.add('invaild')                        
-        form.classList.remove('vaild')                        
+        form.classList.remove('valid')
+        form.classList.add('invalid')
     }
-    if(emalEl == ''){
-        form.classList.remove('vaild')                        
-        form.classList.remove('invaild')                        
+    if(emailEl == ''){
+        form.classList.remove('invalid')
+        form.classList.remove('valid')
     }
+    
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
