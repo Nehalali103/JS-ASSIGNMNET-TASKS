cd34e5dca78659295a03b87d2ef4569e8007fcac
Nehal Ali <nehalalikamran103@gmail.com>
2026-02-07 00:21:45 -0800

    Destructuring

---
+++ Arry ,Obj Destructuring/app.js
@@ -0,0 +1,27 @@
+// let username = 'nehal';
+// let campany = 'Abc Campany';
+// let deParts = 'software';
+
+// Arry Destructuring
+
+// let [username, campany,deParts] = ['nehal', 'Abc Campany', 'Software']
+
+// console.log(username);
+// console.log(campany);
+// console.log(deParts);
+
+
+// obj Destructuring
+
+let obj = {
+    name : 'Huzifa',
+    campany : 'xyz campany',
+    deParts: 'software engnier'
+}
+
+// console.log(obj.name);
+
+
+let {name, campany, deParts} = obj
+
+console.log(name);
