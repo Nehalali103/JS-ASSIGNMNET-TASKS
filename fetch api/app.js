f6510658add88d01178c6189bf0b119a411bcc58
Nehal Ali <nehalalikamran103@gmail.com>
2026-04-07 02:23:15 -0700

    ma ne async await k saad kar api call

--- fetch api/app.js
+++ fetch api/app.js
@@ -3,18 +3,26 @@
 
 let apiCl = 'https://jsonplaceholder.typicode.com/'
 
-let fetchUsersData = () => {
+let fetchUsersData = async () => {
     // console.log(apiCl);
-    fetch(apiCl)
-        .then((succes) => {
-            return succes.json()
-        })
-        .then((actualData) => {
-            console.log('users', actualData);
-        })
-        .catch((err) => {
-            console.log('err', err);
-        })
+    // fetch(apiCl)
+    //     .then((succes) => {
+    //         return succes.json()
+    //     })
+    //     .then((actualData) => {
+    //         console.log('users', actualData);
+    //     })
+    //     .catch((err) => {
+    //         console.log('err', err);
+    //     })
+
+    // let apiRes = await fetch('https://forkify-api.herokuapp.com/api/v2/recipes?search=pizza')
+    // // console.log(apiRes);
+    // let result = await apiRes.json()
+    // console.log(result);
+
+
+    
 }
 
 fetchUsersData()
